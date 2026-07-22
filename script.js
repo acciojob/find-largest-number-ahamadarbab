1b677e32cf6a83be11eba1771b24f82d6ddf6261
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-07-22 23:26:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 function findLargest(a, b, c) {
   //your code here
-	let maxNum = -Infinity;
-
+	
 	if(a >= b && a >= c) {
-		maxNum = a;
+		return a;
 	}
 	else if(b >= a && b >= c) {
-		maxNum = b;
+		return b;
 	}
 	else if(c >= a && c >= b) {
-		maxNum = c;
+		return c;
 	}
 
-	return maxNum
 }
 
-const num1 = parseInt(prompt("Enter First Number."));
-const num2 = parseInt(prompt("Enter Second Number."));
-const num3 = parseInt(prompt("Enter Third Number."));
-alert(findLargest(num1, num2, num3));    
+// const num1 = parseInt(prompt("Enter First Number."));
+// const num2 = parseInt(prompt("Enter Second Number."));
+// const num3 = parseInt(prompt("Enter Third Number."));
+// alert(findLargest(num1, num2, num3));    
 
 
 
