c5cffe895e8ccf7fd76dc047ba89a290d5d64b65
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-07-24 23:05:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ function findLargest(a, b, c) {
 
 }
 
-// const num1 = parseInt(prompt("Enter First Number."));
-// const num2 = parseInt(prompt("Enter Second Number."));
-// const num3 = parseInt(prompt("Enter Third Number."));
-// alert(findLargest(num1, num2, num3));    
+const num1 = parseInt(prompt("Enter First Number."));
+const num2 = parseInt(prompt("Enter Second Number."));
+const num3 = parseInt(prompt("Enter Third Number."));
+alert(findLargest(num1, num2, num3));    
 
 
 
